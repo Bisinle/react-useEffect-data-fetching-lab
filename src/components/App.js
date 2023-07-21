68e01c1d76ec9038d3e388ad33392a53ea838afb
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-21 12:58:17 +0300

    passed all the tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import React from "react";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://dog.ceo/api/breeds/image/random`)
+      .then((res) => res.json())
+      .then((data) => {
+        setImage(data.message);
+        setLoading(false);
+      });
+  }, []);
+
+  console.log(image);
+  return (
+    <div className="App">
+      {loading ? <p>Loading</p> : <img src={image} alt="A Random Dog" />}
+    </div>
+  );
+}
+
+export default App;
